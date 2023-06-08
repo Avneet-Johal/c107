5f0c8acb5d89489fbdf0dffde854c2bc18897263
Avneet-Johal <102407533+Avneet-Johal@users.noreply.github.com>
2023-06-08 16:15:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function startclassification()
+{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('',modelReady);
+}
+function modelReady()
+{
+   classifier.classify(gotResults);
+}
